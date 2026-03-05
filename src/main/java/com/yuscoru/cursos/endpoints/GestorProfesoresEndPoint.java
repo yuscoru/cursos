e67e432efc41e98d61208a58d447ea5f94fc2753
yuscoru <yuscoru@gmail.com>
2026-03-05 22:29:06 +0100

    fix: migrate javax.ws.rs imports to jakarta.ws.rs in GestorProfesoresEndPoint

--- src/main/java/com/yuscoru/cursos/endpoints/GestorProfesoresEndPoint.java
+++ src/main/java/com/yuscoru/cursos/endpoints/GestorProfesoresEndPoint.java
@@ -1,7 +1,29 @@
 package com.yuscoru.cursos.endpoints;
 
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-// Other existing code of GestorProfesoresEndPoint class...
+import org.springframework.stereotype.Component;
 
+import com.yuscoru.cursos.services.ProfesoresService;
+
+@Component
+@Path("/rest/api/v1/profesores")
+public class GestorProfesoresEndPoint {
+
+	private ProfesoresService profesoresService;
+	
+    public GestorProfesoresEndPoint(ProfesoresService profesoresService) {
+		super();
+		this.profesoresService = profesoresService;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response consultaCursos() {
+		return Response.ok().entity(profesoresService.consultaProfesores()).build();
+    }
+}
